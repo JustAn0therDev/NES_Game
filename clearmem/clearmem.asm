b5f02e3badb1eef5b02ebfb0b863bd446ba95519
Ruan Montelo <ruanmontelo@gmail.com>
2024-02-10 19:57:37 -0300

    Changes in the clear mem with init code so the NES starts up in a 'known state'

--- clearmem/clearmem.asm
+++ clearmem/clearmem.asm
@@ -1,3 +1,13 @@
+; Constants for PPU registers mapped from addresses $2000 to $2007
+PPU_CTRL   = $2000
+PPU_MASK   = $2001
+PPU_STATUS = $2002
+OAM_ADDR   = $2003
+OAM_DATA   = $2004
+PPU_SCROLL = $2005
+PPU_ADDR   = $2006
+PPU_DATA   = $2007
+
 ; This is the iNES header (contains a total of 16 bytes with the flags at $7FF0)
 
 .segment "HEADER"
@@ -14,18 +24,27 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; PRG-ROM code located at $8000
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 .segment "CODE"
 
 RESET:
     sei                   ; Disable all IRQ interrupts.
     cld                   ; Clears the decimal-mode (even though the NES's Ricoh 6502 does not have it.)
     ldx #$FF              ; Load the literal $FF hexadecimal value to the X register.
-    txs                   ; Initialize the stack pointer at $01FF.
+    txs                   ; Initialize the stack pointer at $00FF.
+	
+	inx		              ; Overflow X so that -> X (255) + 1 -> X = 0
+	stx PPU_CTRL          ; Disable NMI (PPU_CTRL)
+	stx PPU_MASK          ; Disable rendering (PPU_MASK, masking background and sprites)
+	stx $4010             ; Disable DMC IRQs
+	
+	lda #$40              ; Store the value $#40 into the Y register
+	sta $4017             ; Store the value in the Y register into memory location $4017, disabling the APU frame IRQ.
 
-    lda #$0               ; A = 0
-    inx                   ; X = 0 because it was already $FF before (when initializing the stack pointer) and it overflowed.
+Wait1stVBlank:
+	bit PPU_STATUS        ; Make a bit check
+	bpl Wait1stVBlank     ; Go back while the negative bit isn't 1 (two's complement)
 
+	txa                   ; Transfer the value from X to A.
 ClearRAM:
     sta $0000,x           ; Store the value of A (zero) into $0000 + x (x can go from $0 to $FF)
     sta $0100,x           ; Store the value of A (zero) into $0100 + x (x can go from $0 to $FF)
@@ -37,6 +56,24 @@ ClearRAM:
     sta $0700,x           ; Store the value of A (zero) into $0700 + x (x can go from $0 to $FF)
     inx                   ; X--
     bne ClearRAM          ; If X is not zero, we loop back to the ClearRAM label.
+
+Wait2ndVBlank:            ; Wait for the second VBlank from the PPU
+    bit PPU_STATUS        ; Perform a bit-wise check with the PPU_STATUS port
+    bpl Wait2ndVBlank     ; Loop until bit-7 (sign bit) is 1 (inside VBlank)
+	
+Main:
+	ldx #$3F              ; Store the value #$3F into the X register.
+	stx PPU_ADDR          ; Load the value in X into PPU_ADDR register, setting the hi-byte
+	
+	ldx #$00              ; Store the value #$00 into the X register.
+	stx PPU_ADDR          ; Load the value in X into PPU_ADDR register, setting the lo-byte
+	
+	ldx #$2A              ; Load the value #$2A into the X register.
+	stx PPU_DATA          ; Load the value in X into PPU_DATA, setting the value at #$3F00 to 2A 
+						  ; (first byte of the 16 bytes for the background color palette)
+	
+	ldx #%00011110        ; Load the value #%00011110 into X.
+	stx PPU_MASK          ; Load the value in X into the PPU_MASK register, allowing the background to be rendered.
 
 NMI:
     rti ; Return, don't do anything
