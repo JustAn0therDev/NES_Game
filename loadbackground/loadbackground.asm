6e0511a0ded1e0cbe428cc24a136d855e3a17dd9
Ruan Montelo <ruanmontelo@gmail.com>
2024-03-13 18:58:36 -0300

    Drawing the full nametable

--- loadbackground/loadbackground.asm
+++ loadbackground/loadbackground.asm
@@ -15,38 +15,54 @@ BgPtr: .res 2            ; Reserve 2 bytes (16 bits) to store a pointer to the b
 .segment "CODE"
 
 ;; Load color palette
-.proc LoopPalette
+.proc LoadPalette
 	ldy #0
-LoopPalette:
+LoadPalette:
 	lda PaletteData, y    ; Load the A register with the value in the exact memory region where we keep the color palette bytes.
 	sta PPU_DATA          ; Load the PPU_DATA register with the value that is inside the A register.
 	iny                   ; Increment Y
 	cpy #32               ; Compare it to 32, it being past the last byte we want to access before reaching the end of the color palette.
-	bne LoopPalette       ; If not equal, loop back to the LoopPalette label.
+	bne LoadPalette       ; If not equal, loop back to the LoadPalette label.
 	rts
 .endproc
 
 ;; Load background tiles
-.proc LoopBackground
-	ldy #0
-LoopBackground:
-	lda (BgPtr), y 				; Load the A register with the value in the exact memory region where we keep the background data bytes.
+.proc LoadBackground
+	lda #<BackgroundData  ; Fetch the lo-byte of the BackgroundData address
+	sta BgPtr
+	lda #>BackgroundData  ; Fetch the hi-byte of the BackgroundData address
+	sta BgPtr+1
+	
+	PPU_SETADDR $2000     ; Set PPU address to $2000 (start of the nametables), and load the tiles of the background
+	
+	ldx #$00              ; Set X register to 0
+	ldy #$00              ; Set Y register to 0
+
+OuterLoop:
+InnerLoop:
+	lda (BgPtr),y 				; Load the A register with the value in the exact memory region where we keep the background data bytes.
 	sta PPU_DATA                ; Load the PPU_DATA register with the value that is inside the A register.
 	iny                         ; Increment Y
-	cpy #$FF                    ; Compare it to $FF, it being past the last byte we want to access before reaching the end of the background.
-	bne LoopBackground          ; If not equal, loop back to the LoopBackground label.
+	cpy #$0                     ; Compare it to #$0, checking for overflow
+	beq IncrementHiByte         ; If equal to zero, jump to IncrementHiByte.
+	jmp InnerLoop
+IncrementHiByte:
+	inc BgPtr+1                 ; Increment the hi-byte value in the BgPtr+1 memory region
+	inx                         ; Increment X
+	cpx #4                      ; Compare it to 4
+	bne OuterLoop               ; If it is not equal to 4, jump back to OuterLoop
 	rts
 .endproc
 
 ;; Load attribute bytes
-.proc LoopAttribute
+.proc LoadAttribute
 	ldy #0
-LoopAttribute:
+LoadAttribute:
 	lda AttributeData, y        ; Load the A register with the memory location of AttributeData and the value in the Y register.
 	sta PPU_DATA                ; Set the value in the A register to the memory location of PPU_DATA (where we want to load the attribute values.)
 	iny                         ; Increment Y
 	cpy #16                     ; Compare the value in the Y register to #16.
-	bne LoopAttribute           ; If it's not equal, loop back to the LoopAttribute label.
+	bne LoadAttribute           ; If it's not equal, loop back to the LoadAttribute label.
 	rts
 .endproc
 
@@ -62,28 +78,23 @@ Reset:
 	
 Main:
 	PPU_SETADDR $3F00
-	jsr LoopPalette       ; Jump to the subroutine that fills the color palette in the PPU
 
-	; Set PPU address to $2000, and load the tiles of the background
-	
-	lda #<BackgroundData  ; Fetch the lo-byte of the BackgroundData address
-	sta BgPtr
-	lda #>BackgroundData  ; Fetch the hi-byte of the BackgroundData address
-	sta BgPtr+1
-
-	PPU_SETADDR $2000
-	jsr LoopBackground    ; Jump to the subroutine that loads the background data into the PPU.
+	jsr LoadPalette       ; Jump to the subroutine that fills the color palette in the PPU
+	jsr LoadBackground    ; Jump to the subroutine that loads the background data into the PPU.
 
 	; Set PPU address to $23C0, and load the attributes of the background.
 
 	PPU_SETADDR $23C0
-	jsr LoopAttribute     ; Jump to the subroutine that loads the background color data into the PPU.
+	jsr LoadAttribute     ; Jump to the subroutine that loads the background color data into the PPU.
 
 EnablePPURendering:
-	lda #%10010000        ; Enable NMI and set background to use the 2nd pattern table (at $1000)
-	sta PPU_CTRL
-	ldx #%00011110        ; Load the value #%00011110 into X.
-	stx PPU_MASK          ; Load the value in X into the PPU_MASK register, allowing the background to be rendered.
+    lda #%10010000           ; Enable NMI and set background to use the 2nd pattern table (at $1000)
+    sta PPU_CTRL
+    lda #0
+    sta PPU_SCROLL           ; Disable scroll in X
+    sta PPU_SCROLL           ; Disable scroll in Y
+    lda #%00011110
+    sta PPU_MASK             ; Set PPU_MASK bits to render the background
 	
 LoopForever:
 	jmp LoopForever       ; Force an infinite execution loop.
@@ -108,18 +119,52 @@ PaletteData:
 .byte $22,$29,$1A,$0F, $22,$36,$17,$0F, $22,$30,$21,$0F, $22,$27,$17,$0F ; Background palette
 .byte $22,$16,$27,$18, $22,$1A,$30,$27, $22,$16,$30,$27, $22,$0F,$36,$17 ; Sprite palette
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Background data with tile numbers that must be copied to the nametable
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 BackgroundData:
-.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$36,$37,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
-.byte $24,$24,$24,$24,$24,$24,$24,$24,$35,$25,$25,$38,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$60,$61,$62,$63,$24,$24,$24,$24
-.byte $24,$36,$37,$24,$24,$24,$24,$24,$39,$3a,$3b,$3c,$24,$24,$24,$24,$53,$54,$24,$24,$24,$24,$24,$24,$64,$65,$66,$67,$24,$24,$24,$24
-.byte $35,$25,$25,$38,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$55,$56,$24,$24,$24,$24,$24,$24,$68,$69,$26,$6a,$24,$24,$24,$24
-.byte $45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45,$45
-.byte $47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47
-.byte $47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47,$47
-.byte $00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00,$00
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$36,$37,$36,$37,$36,$37,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$35,$25,$25,$25,$25,$25,$25,$38,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$36,$37,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$39,$3A,$3B,$3A,$3B,$3A,$3B,$3C,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$35,$25,$25,$38,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$39,$3A,$3B,$3C,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$53,$54,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$55,$56,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$53,$54,$24,$24,$24,$24,$24,$24,$24,$24,$45,$45,$53,$54,$45,$45,$53,$54,$45,$45,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$55,$56,$24,$24,$24,$24,$24,$24,$24,$24,$47,$47,$55,$56,$47,$47,$55,$56,$47,$47,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$60,$61,$62,$63,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$24,$31,$32,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$24,$64,$65,$66,$67,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$24,$30,$26,$34,$33,$24,$24,$24,$24,$36,$37,$36,$37,$24,$24,$24,$24,$24,$24,$24,$68,$69,$26,$6A,$24,$24,$24,$24
+.byte $24,$24,$24,$24,$30,$26,$26,$26,$26,$33,$24,$24,$35,$25,$25,$25,$25,$38,$24,$24,$24,$24,$24,$24,$68,$69,$26,$6A,$24,$24,$24,$24
+.byte $B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5
+.byte $B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7
+.byte $B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B6
+.byte $B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7
+.byte $B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5,$B4,$B5
+.byte $B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7,$B6,$B7
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Attributes tell which palette is used by a group of tiles in the nametable
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 AttributeData:
-.byte %00000000, %00000000, %10101010, %00000000, %11110000, %00000000, %00000000, %00000000
+.byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+.byte %00000000, %10101010, %10101010, %00000000, %00000000, %00000000, %10101010, %00000000
+.byte %00000000, %00000000, %00000000, %00000000, %11111111, %00000000, %00000000, %00000000
+.byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+.byte %11111111, %00000000, %00000000, %00001111, %00001111, %00000011, %00000000, %00000000
+.byte %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000, %00000000
+.byte %11111111, %11111111, %11111111, %11111111, %11111111, %11111111, %11111111, %11111111
 .byte %11111111, %11111111, %11111111, %11111111, %11111111, %11111111, %11111111, %11111111
 
 ; Here we add the CHR-ROM data, included from an external .CHR file.
