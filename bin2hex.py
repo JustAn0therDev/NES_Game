76676d287993f612e4074308ded0f347912c557c
Ruan Montelo <ruanmontelo@gmail.com>
2024-03-24 10:37:12 -0300

    Added bin2hex.py file to convert background files to hex data, to be used in the cartridge

---
+++ bin2hex.py
@@ -0,0 +1,12 @@
+import sys
+
+with open(sys.argv[1], 'rb') as f:
+  i = 0
+  while True:
+    hexdata = f.read(1).hex()
+    if len(hexdata) == 0:
+      break
+    if i % 32 == 0:
+      print()
+    print('$' + hexdata.upper(), end = ',')
+    i = i + 1
